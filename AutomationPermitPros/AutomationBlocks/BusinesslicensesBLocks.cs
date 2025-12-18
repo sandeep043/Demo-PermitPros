a4b4ea444ac264dbe84e6336b4333fa051563fe8
Jagan Pambala <jagan.p@prudentconsulting.com>
2025-12-18 17:01:39 +0530

    updated generic blocks

--- AutomationPermitPros/AutomationBlocks/BusinesslicensesBLocks.cs
+++ AutomationPermitPros/AutomationBlocks/BusinesslicensesBLocks.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AutomationPermitPros.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,58 @@ using System.Threading.Tasks;
 
 namespace AutomationPermitPros.AutomationBlocks
 {
-    internal class BusinesslicensesBLocks
+    public class BusinesslicensesBLocks
     {
+        private readonly BusinessLicensesPage _page;
+
+        public BusinesslicensesBLocks(BusinessLicensesPage page)
+        {
+            _page = page;
+        }
+
+        //Create New Button
+        public async Task<bool> BUSLIC_CREATE_NEW()
+        {
+            try
+            {
+                await _page.ClickCreateNew();
+                return await _page.IsCreatePageLoaded();
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        //Search Button
+        public async Task<bool> BUSLIC_SearchButton()
+        {
+            try
+            {
+                await _page.ClickSearch();
+                return await _page.IsCreatePageLoaded();
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        //Export to Excel
+        public async Task<bool> EXPORT_EXCEL()
+        {
+            try
+            {
+                var filePath = await _page.ExportToExcel();
+                return File.Exists(filePath);
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+
+
     }
 }
+
---
+++ AutomationPermitPros/Pages/Base/BaseListPage.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Playwright;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationPermitPros.Pages.Base
+{
+    public abstract class BaseListPage
+    {
+        protected readonly IPage _page;
+
+        protected BaseListPage(IPage page) { 
+            _page = page; 
+
+        }
+        protected ILocator CreateNewButton =>
+            _page.Locator("button:has-text('Create New')"); 
+        
+            protected ILocator SearchButton =>
+                _page.GetByRole(AriaRole.Button, new() { Name = "Search" });
+
+        protected ILocator ExportToExcelButton =>
+            _page.GetByRole(AriaRole.Button,new() { Name = "Export to Excel" });
+
+        public async Task ClickCreateNew()
+        {
+            await CreateNewButton.ClickAsync();
+            await _page.WaitForLoadStateAsync(LoadState.NetworkIdle);
+        }
+
+        public async Task ClickSearch()
+        {
+            await SearchButton.ClickAsync();
+            await _page.WaitForLoadStateAsync(LoadState.NetworkIdle);
+        }
+
+
+        public async Task<string> ExportToExcel()
+        {
+            var download = await _page.RunAndWaitForDownloadAsync(async () =>
+            {
+                await ExportToExcelButton.ClickAsync();
+            });
+
+            var filePath = Path.Combine(
+                Directory.GetCurrentDirectory(),
+                "Downloads",
+                $"Tasks_{DateTime.Now:yyyyMMdd_HHmmss}.xlsx"
+            );
+
+            await download.SaveAsAsync(filePath);
+            return filePath;
+        }
+    }
+}
+
