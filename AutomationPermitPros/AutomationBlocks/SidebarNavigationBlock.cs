fea5af1a384e80b09d01b58d78b5b87c3de87255
Annapurna S K <annapurna.sk@prudentconsulting.com>
2025-12-19 16:44:27 +0530

    added side navigation Bar

---
+++ AutomationPermitPros/AutomationBlocks/SidebarNavigationBlock.cs
@@ -0,0 +1,33 @@
+﻿using AutomationPermitPros.Pages;
+using Microsoft.Playwright;
+
+
+namespace AutomationPermitPros.AutomationBlocks
+{
+
+    public class SidebarNavigationBlock
+    {
+        private readonly SidebarPage _sidebarPage;
+
+        public SidebarNavigationBlock(IPage page)
+        {
+            _sidebarPage = new SidebarPage(page);
+        }
+
+
+        public async Task<bool> NavigateToAsync(string menuName)
+        {
+            try
+            {
+                await _sidebarPage.ClickMenuAsync(menuName);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[NavigateToAsync] Error navigating to '{menuName}': {ex.Message}");
+                Console.WriteLine(ex.StackTrace);
+                return false;
+            }
+        }
+    }
+}
